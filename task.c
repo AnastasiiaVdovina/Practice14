1c94f9971f7f8bac97b5e37c243aea4d565d1e16
Anastasiia Vdovina <144135681+AnastasiiaVdovina@users.noreply.github.com>
2025-06-03 14:35:25 +0300

    Add files via upload

---
+++ task.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <signal.h>
+#include <string.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+volatile sig_atomic_t tick_count = 0 ;
+
+
+void handler(int sig, siginfo_t *si, void *uc){
+	tick_count++;
+
+	char buf[64];
+	int len = snprintf(buf, sizeof(buf), "Tick: %ld\n", tick_count);
+	write(STDOUT_FILENO, buf , len);
+	
+}
+
+int main(){
+	struct sigaction sa = {0};
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = handler;
+	sigaction(SIGRTMIN, &sa, NULL);
+
+	timer_t timerid;
+	struct sigevent sev = {0};
+
+	sev.sigev_notify = SIGEV_SIGNAL;
+	sev.sigev_signo = SIGRTMIN;
+
+	timer_create(CLOCK_REALTIME, &sev, &timerid);
+
+	struct itimerspec its;
+	its.it_value.tv_sec = 1;
+	its.it_value.tv_nsec = 0;
+	its.it_interval.tv_sec = 1;
+	its.it_interval.tv_nsec = 0;
+	
+	timer_settime(timerid, 0, &its, NULL);
+	
+	while(1)
+		pause();
+	
+}
